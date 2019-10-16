b482930eb5514ef812cc57027f70d5d52c27cb15
Thomas Helton <thomas@heltonfamily.info>
2019-10-15 20:57:59 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const auth = require('./auth.json');
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -17,4 +16,4 @@ client.on('message', msg => {
   });
 
 
-client.login(auth.token);
+client.login(process.env.BOT_TOKEN);
